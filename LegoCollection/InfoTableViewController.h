7a2b9b2584e68ee6f4bf87ba5b4184a3cb67afa9
Morgan Davison <morgan.davison@gmail.com>
2017-03-16 05:56:56 -0700

    Add icons for tab bar

---
+++ LegoCollection/InfoTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  InfoTableViewController.h
+//  LegoCollection
+//
+//  Created by Morgan Davison on 3/15/17.
+//  Copyright © 2017 Morgan Davison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoTableViewController : UITableViewController
+
+- (IBAction)openIcons8Link:(UIButton *)sender;
+
+@end
---
+++ LegoCollection/InfoTableViewController.m
@@ -0,0 +1,45 @@
+//
+//  InfoTableViewController.m
+//  LegoCollection
+//
+//  Created by Morgan Davison on 3/15/17.
+//  Copyright © 2017 Morgan Davison. All rights reserved.
+//
+
+#import "InfoTableViewController.h"
+
+@interface InfoTableViewController ()
+
+@end
+
+@implementation InfoTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+
+
+
+#pragma mark - Actions
+
+- (IBAction)openIcons8Link:(UIButton *)sender {
+    NSLog(@"open the icons 8 link: %@", sender.titleLabel.text);
+    NSURL *url = [NSURL URLWithString:sender.titleLabel.text];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+@end
